e70de1debd742493c210428f576939662fb618f6
TAMIR YARDENI <tamir.yardeni1@gmail.com>
2018-08-04 11:33:39 +0300

    Added connection to web service - show the news in a bar

--- client/src/app/dashboard/dashboard.component.ts
+++ client/src/app/dashboard/dashboard.component.ts
@@ -10,12 +10,27 @@ import { Router } from '@angular/router';
 export class DashboardComponent implements OnInit {
 
   public currentUser : any = {};
-
+  newsTxt;
   constructor(private userService: UserService, private router: Router) { }
 
   ngOnInit() {
     this.userService.getCurrentUser().then(profile => this.currentUser = profile)
         .catch(() => this.currentUser = {});
+    this.newsTxt = "";
+    var url = 'https://newsapi.org/v2/top-headlines?' +
+      'country=us&' +
+      'apiKey=6ee435df1f0d47439b3223b89e227267';
+    var req = new Request(url);
+    fetch(req).then((response) => {
+      var txt = "";
+      response.json().then(response => {
+          response['articles'].forEach(element => {
+            txt += element.title + " | ";
+          });
+          debugger;
+          this.newsTxt = txt;
+      });
+    });
   }
 
   logout() {
